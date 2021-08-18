345cb4819896af028f18a6e9f2cf007a80072cd0
Colo <davidsupermario@gmail.com>
2021-08-18 02:28:46 -0300

    V0.2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let nombre = "David";
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <label htmlFor="nombre">Nombre</label>
+        <input type = "text" id= "nombre"/>
+        <h1>(nombre)</h1>
         <p>
           Editar <code>src/App.js</code> y guarda para recargar.
         </p>
@@ -19,6 +24,7 @@ function App() {
         </a>
       </header>
     </div>
+    </>
   );
 }
 
